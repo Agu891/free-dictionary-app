977ae1f0171a345841847ddd70ab4f43c160869c
Agustin <agu89.1@gmail.com>
2022-07-18 12:05:16 -0300

    arreglos en el audio

--- main.js
+++ main.js
@@ -1,48 +1,53 @@
-const url = 'https://api.dictionaryapi.dev/api/v2/entries/en'
-const form = document.querySelector("#form")
-const definition = document.querySelector("#definition")
-const input = document.querySelector("#texto")
-let tituloPalabra = document.querySelector("#tituloPalabra")
-let significados = []
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en";
+const form = document.querySelector("#form");
+const definition = document.querySelector("#definition");
+const input = document.querySelector("#texto");
+let tituloPalabra = document.querySelector("#tituloPalabra");
+let significados = [];
 
-  
-form.addEventListener('submit', async (event)=>{
-    event.preventDefault()
-    let inputValue = input.value
-    let datos = []
-    
-    
-    if(input){
-        let response =  await fetch(`${url}/${inputValue}`)
-        let palabras = await response.json()
-        datos = palabras
-      
-    } 
-    definition.innerHTML = ""
-        
-     for(let i= 0 ; i < datos.length; i++){
-        
-        let word = datos[i].word.toUpperCase()
-       
-         for(let j= 0; j < datos[i].meanings.length; j++){
-            let partOfSpeech = datos[i].meanings[j].partOfSpeech
-            let phonetics = datos[i].phonetics[j].text
-            let audio = datos[i].phonetics[j].audio
-            
-            
-            for(let k = 0; k < datos[i].meanings[j].definitions.length; k++ ){
-                       definition.innerHTML += ` <ul><li> Word : ${word} </li>
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  let inputValue = input.value;
+  let datos = [];
+
+  if (input) {
+    let response = await fetch(`${url}/${inputValue}`);
+    let palabras = await response.json();
+    datos = palabras;
+  }
+  definition.innerHTML = "";
+
+  for (let i = 0; i < datos.length; i++) {
+    let word = datos[i].word.toUpperCase();
+
+    for (let j = 0; j < datos[i].meanings.length; j++) {
+      let partOfSpeech = datos[i].meanings[j].partOfSpeech;
+      let phonetics = datos[i].phonetics[j].text;
+      let audio = datos[i].phonetics[j].audio;
+
+      for (let k = 0; k < datos[i].meanings[j].definitions.length; k++) {
+        definition.innerHTML += ` <ul><li> Word : ${word} </li>
                                                <li> Phonetic : ${phonetics} </li>
                                                <li> Word type : ${partOfSpeech} </li>
-                                               <li> Definition: ${datos[i].meanings[j].definitions[k].definition} </li>
-                                               <li> Examples : ${datos[i].meanings[j].definitions[k].example || 'No hay ejemplos disponibles'} </li> 
-                                               <li id="lastLi"> ${audio ? `<a href="${audio}" ><button id="audioBtn">Audio</button><a/>`: "No hay audios disponibles"}</li></ul>`     
-             }
-         }
-         }
-  if(definition.innerHTML.length === 0){
-      alert("La palabra que estas buscando no existe")
-      input.value = "" 
+                                               <li> Definition: ${
+                                                 datos[i].meanings[j]
+                                                   .definitions[k].definition
+                                               } </li>
+                                               <li> Examples : ${
+                                                 datos[i].meanings[j]
+                                                   .definitions[k].example ||
+                                                 "No hay ejemplos disponibles"
+                                               } </li> 
+                                               <li id="lastLi"> ${
+                                                 audio
+                                                   ? `<audio src="${audio}" controls>Play me</audio>`
+                                                   : "No hay audios disponibles"
+                                               }</li></ul>`;
+      }
+    }
   }
-    
-})
+  if (definition.innerHTML.length === 0) {
+    alert("La palabra que estas buscando no existe");
+    input.value = "";
+  }
+});
